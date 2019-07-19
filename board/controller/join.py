04be3cb4c08328a62cc5953c4de3476049097520
DONGBUM, KIM <dongbum9@gmail.com>
2019-07-19 12:19:34 +0900

    insert 추가

--- board/controller/join.py
+++ board/controller/join.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template, request
+import pymysql
+from flask import render_template, request, current_app
 from board.board_blueprint import board
 
 @board.route('/join')
@@ -14,5 +15,26 @@ def join_process():
     email = request.form['email']
 
     print('id:[%s] password:[%s] email:[%s]' % (id, password, email))
+
+    db_address = current_app.config['DB_ADDRESS']
+    db_port = current_app.config['DB_PORT']
+    db_id = current_app.config['DB_ID']
+    db_password = current_app.config['DB_PASSWORD']
+    db_name = current_app.config['DB_NAME']
+
+    conn = pymysql.connect(host=db_address,
+                           port=int(db_port),
+                           user=db_id,
+                           password=db_password,
+                           db=db_name,
+                           charset='utf8')
+
+    try:
+        cursor = conn.cursor()
+        sql = "INSERT INTO users(id, password, email) VALUES('%s', '%s', '%s')" % (id, password, email)
+        cursor.execute(sql)
+        conn.commit()
+    finally:
+        conn.close()
 
     return render_template('join.html')
