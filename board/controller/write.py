e771b301bdbb2de31a8c47cec11a2bebf410dda7
DONGBUM, KIM <dongbum9@gmail.com>
2019-07-22 14:44:38 +0900

    글 입력 기능 구현

--- board/controller/write.py
+++ board/controller/write.py
@@ -4,6 +4,40 @@ import pymysql
 from flask import render_template, request, current_app
 from board.board_blueprint import board
 
-@board.route('/write')
+@board.route('/write', methods=['GET', 'POST'])
 def write():
+    if request.method == 'POST':
+        writer = request.form['writer']
+        content = request.form['content']
+
+        if writer and content:
+            print('POST writer:[%s] content:[%s]' % (writer, content))
+        else:
+            return render_template('write.html')
+
+        db_address = current_app.config['DB_ADDRESS']
+        db_port = current_app.config['DB_PORT']
+        db_id = current_app.config['DB_ID']
+        db_password = current_app.config['DB_PASSWORD']
+        db_name = current_app.config['DB_NAME']
+
+        conn = pymysql.connect(host=db_address,
+                               port=int(db_port),
+                               user=db_id,
+                               password=db_password,
+                               db=db_name,
+                               charset='utf8')
+
+        try:
+            cursor = conn.cursor()
+            sql = "INSERT INTO board(writer, content) VALUES(%s, '%s')" % (writer, content)
+            print(sql)
+            cursor.execute(sql)
+            conn.commit()
+        finally:
+            conn.close()
+
+    else:
+        print('NULL')
+
     return render_template('write.html')
